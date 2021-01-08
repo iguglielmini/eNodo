cdc063719c0fe3b0b5534bc963ac3187e32c477f
Isaac Henrique <isaac@epicsoftwarehouse.com>
2021-01-08 13:20:42 -0300

    Ajustes

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -44,36 +44,7 @@ class Cart extends Component {
       currentScrollY: 0,
       scrollY: new Animated.Value(0),
       cart: {
-        items: [
-          {
-            product: {
-              id: 7482,
-              sku: 1235,
-              url:
-                "l-oreal-professionnel-shampoo-absolut-repair-lipidium-300ml",
-              slug:
-                "l-oreal-professionnel-shampoo-absolut-repair-lipidium-300ml-p7482",
-              title:
-                "L'Oréal Professionnel Shampoo Absolut Repair Lipidium 300ml",
-              image:
-                "https://app-assets.belshop.com.br/l-oreal-professionnel-shampoo-absolut-repair-lipidium-300ml-p7482.png",
-              link: "http://api-gateway.belshop.com.br/product/7482",
-              price: {
-                current: 79.9,
-                previous: 99.9,
-                discount: 20,
-                installments: 4,
-                installmentPrice: 19.98,
-              },
-            },
-            id: 123,
-            sku: 456,
-            quantity: 2,
-            basketItemId: 789,
-            itemPrice: 19.9,
-            totalPrice: 39.8,
-          },
-        ],
+        items: [],
         totalPrice: 0.0,
       },
     };
@@ -138,8 +109,6 @@ class Cart extends Component {
       outputRange: [-0, 60, 60, 60],
       extrapolate: "clamp",
     });
-
-    console.log("PICA ", cart);
 
     return (
       <>
