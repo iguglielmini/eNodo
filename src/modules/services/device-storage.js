05b83f77aa3e1dd70eb7e09730a4c6933c1b8543
Isaac Henrique <isaac@epicsoftwarehouse.com>
2021-01-08 12:59:40 -0300

    Ajustes

--- src/modules/services/device-storage.js
+++ src/modules/services/device-storage.js
@@ -1,4 +1,5 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AsyncStorage } from 'react-native';
 
 class DeviceStorage {
   async getTotalCart() {
